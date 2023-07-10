2b18486609f7c280279c90d20281a777776814e3
fiZZowsky <piotrekr53@interia.pl>
2023-07-10 11:35:50 +0200

    added categories and subcategories endpoints

---
+++ PhoneBook.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhoneBook.Api.Entities;
+using PhoneBook.Api.Repositories.Contracts;
+
+namespace PhoneBook.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly IContactRepository contactRepository;
+
+        public CategoriesController(IContactRepository contactRepository)
+        {
+            this.contactRepository = contactRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
+        {
+            try
+            {
+                var categories = await this.contactRepository.GetCategories();
+
+                if(categories == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok(categories);
+                }
+            }
+            catch(Exception) 
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from database");
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Category>> GetCategory(int id)
+        {
+            try
+            {
+                var category = await this.contactRepository.GetCategory(id);
+
+                if(category == null)
+                {
+                    return BadRequest();
+                }
+                else
+                {
+                    return Ok(category);
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from database");
+            }
+        }
+    }
+}
---
+++ PhoneBook.Api/Controllers/SubcategoriesController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhoneBook.Api.Entities;
+using PhoneBook.Api.Repositories.Contracts;
+
+namespace PhoneBook.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubcategoriesController : ControllerBase
+    {
+        private readonly IContactRepository contactRepository;
+
+        public SubcategoriesController(IContactRepository contactRepository)
+        {
+            this.contactRepository = contactRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Subcategory>>> GetSubcategories()
+        {
+            try
+            {
+                var subcategories = await this.contactRepository.GetSubcategories();
+
+                if (subcategories == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok(subcategories);
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from database");
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Subcategory>> GetSubcategory(int id)
+        {
+            try
+            {
+                var subcategory = await this.contactRepository.GetSubcategory(id);
+
+                if (subcategory == null)
+                {
+                    return BadRequest();
+                }
+                else
+                {
+                    return Ok(subcategory);
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from database");
+            }
+        }
+    }
+}
